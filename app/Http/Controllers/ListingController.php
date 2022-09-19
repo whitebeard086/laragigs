7aec4983cc5b91f4314b215e89dd49d5d614924d
The Exorcist <whitebeard086@gmail.com>
2022-09-19 19:55:57 +0100

    Edit Listing

--- app/Http/Controllers/ListingController.php
+++ app/Http/Controllers/ListingController.php
@@ -47,4 +47,30 @@ class ListingController extends Controller
 
         return redirect('/')->with('success', 'Listing created successfully!');
     }
+
+    // Show Edit Form
+    public function edit(Listing $listing) {
+        return view('listing.edit', ['listing' => $listing]);
+    }
+
+    // Update Listing Data
+    public function update(Request $request, Listing $listing) {
+        $formFields = $request->validate([
+            'title' => 'required',
+            'company' => ['required'],
+            'location' => 'required',
+            'website' => 'required',
+            'email' => ['required', 'email'],
+            'tags' => 'required',
+            'description' => 'required',
+        ]);
+
+        if($request->hasFile('logo')) {
+            $formFields['logo'] = $request->file('logo')->store('logos', 'public');
+        }
+
+        $listing->update($formFields);
+
+        return back()->with('success', 'Listing updated successfully!');
+    }
 }
--- routes/web.php
+++ routes/web.php
@@ -35,7 +35,11 @@ Route::get('/listings/create', [ListingController::class, 'create']);
 // Store Listing Data
 Route::post('/listings', [ListingController::class, 'store']);
 
+// Show Edit Form
+Route::get('/listings/{listing}/edit', [ListingController::class, 'edit']);
 
+// Submit Edit Form to Update Listing
+Route::put('/listings/{listing}', [ListingController::class, 'update']);
 
 // Single Listing
 Route::get('/listings/{listing}', [ListingController::class, 'show']);
