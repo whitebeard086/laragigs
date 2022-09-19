f78c61fc815463e860c007b89e6f014a60014366
The Exorcist <whitebeard086@gmail.com>
2022-09-19 07:38:12 +0100

    Component Attributes

--- resources/views/listing.blade.php
+++ resources/views/listing.blade.php
@@ -7,7 +7,7 @@
 ><i class="fa-solid fa-arrow-left"></i> Back
 </a>
 <div class="mx-4">
-    <x-card>
+    <x-card class="p-10">
         <div
             class="flex flex-col items-center justify-center text-center"
         >
